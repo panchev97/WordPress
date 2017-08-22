64b2cfca022e1742e0d7845e92304b2282e1d4ed
Rumen Panchev <rpanchev9708@gmail.com>
2017-08-22 12:46:55 +0300

    Added new option for selecting the post type

--- wordpress/wp-content/plugins/ru-plugin/inc/ru-widget-class.php
+++ wordpress/wp-content/plugins/ru-plugin/inc/ru-widget-class.php
@@ -29,7 +29,7 @@
 
        $posts_args = array(
          'post_status'    => 'publish',
-         'post_type'      => 'post',
+         'post_type'      => $instance['post_type_dropdown'],
          'posts_per_page' => $instance['posts_number'],
          'orderby'        => 'title',
          'order'          => $instance['order_posts_dropdown'],
@@ -79,9 +79,10 @@
      public function update( $new_instance, $old_instance ) {
        $instance = $old_instance;
 
-       $instance['title']           = strip_tags($new_instance['title']);
-       $instance['posts_number']     = strip_tags($new_instance['posts_number']);
-       $instance['order_posts_dropdown'] = strip_tags($new_instance['order_posts_dropdown']);
+       $instance['title']                = strip_tags( $new_instance['title'] );
+       $instance['posts_number']         = strip_tags( $new_instance['posts_number'] );
+       $instance['order_posts_dropdown'] = strip_tags( $new_instance['order_posts_dropdown'] );
+       $instance['post_type_dropdown']   = strip_tags( $new_instance['post_type_dropdown'] );
 
        return $instance;
      }
@@ -94,6 +95,7 @@
             'title'                => 'Title',
             'posts_number'         => '1',
             'order_posts_dropdown' => 'asc',
+            'post_type_dropdown'   => 'post',
         );
 
         $instance = wp_parse_args( $instance, $instance_defaults );
@@ -101,6 +103,7 @@
         $title                = esc_attr( $instance['title'] );
         $posts_number         = esc_attr( $instance['posts_number'] );
         $order_posts_dropdown = esc_attr( $instance['order_posts_dropdown'] );
+        $post_type_dropdown   = esc_attr( $instance['post_type_dropdown'] );
 
             ?>
         <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( "Title:", 'rubase'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
@@ -110,6 +113,13 @@
           <select name="<?php echo $this->get_field_name('order_posts_dropdown'); ?>" id="<?php echo $this->get_field_id('order_posts_dropdown'); ?>" class="widefat">
             <option value="asc"<?php selected( $instance['order_posts_dropdown'], 'asc' ); ?>><?php _e( "Ascending", 'rubase' ); ?></option>
             <option value="desc"<?php selected( $instance['order_posts_dropdown'], 'desc' ); ?>><?php _e( "Descending", 'rubase' ); ?></option>
+          </select>
+        </p>
+        <p>
+          <label for="<?php echo $this->get_field_id('post_type_dropdown'); ?>"><?php _e( "Select a post type", 'rubase' ); ?></label>
+          <select name="<?php echo $this->get_field_name('post_type_dropdown'); ?>" id="<?php echo $this->get_field_id('post_type_dropdown'); ?>" class="widefat">
+            <option value="post"<?php selected( $instance['post_type_dropdown'], 'post' ); ?>><?php _e( "Default Post Type", 'rubase' ); ?></option>
+            <option value="custom_posts"<?php selected( $instance['post_type_dropdown'], 'custom_posts' ); ?>><?php _e( "Custom Post Type", 'rubase' ); ?></option>
           </select>
         </p>
       <?php
