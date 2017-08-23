f64f09bd56ecbf451e366617421c2021f4608302
Rumen Panchev <rpanchev9708@gmail.com>
2017-08-23 13:15:44 +0300

    Added testimonials plugin

---
+++ wordpress/wp-content/plugins/ru-testimonials-plugin/inc/ru-testimonials-widget-class.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Widget initialization
+ * @author Rumen Panchev
+ */
+ class RU_Testimonials_Widget extends WP_Widget {
+   public function __construct() {
+        parent::__construct(
+            'ru_testimonials_widget',
+            __("RU Testimonials Widget", 'rubase'),
+            array( 'classname' => 'ru_testimonials_widget_sample_single', 'description' => __( "Widget for displaying testimonials to your posts/pages", 'rubase' ) )
+        );
+    }
+
+    /**
+     * Output of widget
+     */
+    public function widget( $args, $instance ) {
+      extract( $args );
+
+      $testimonials_args = array(
+        'post_status'    => 'publish',
+        'post_type'      => 'testimonials',
+        'posts_per_page' => $instance['testimonials_number'],
+        'orderby'        => 'title',
+        'order'          => $instance['order_testimonials_dropdown'],
+      );
+      $query = new WP_Query( $testimonials_args );
+      ?>
+      <div class="wrapper">
+        <h1><?php echo $instance['title'] ?></h1>
+        <?php if ( $query->have_posts() ) :
+          while ( $query->have_posts() ) : $query->the_post() ?>
+            <ul>
+              <li>
+                <div class="post">
+                  <a href="<?php the_permalink() ?>"
+                    <h3 class="title">
+                      <a href="<?php the_permalink() ?>" title=""><b><?php echo the_title() ?></b></a>
+                    </h3>
+                    <p><?php the_excerpt() ?></p>
+                </div>
+              </li>
+            </ul>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+
+      <?php
+     // Get widget field values
+     $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+
+
+     // End sample widget body creation
+
+     if ( !empty( $out ) ) {
+       echo $before_widget;
+       if ( $title ) {
+         echo $before_title . $title . $after_title;
+       }
+       ?>
+         <div>
+           <?php echo $out; ?>
+         </div>
+       <?php
+         echo $after_widget;
+      }
+    }
+
+    public function update( $new_instance, $old_instance ) {
+      $instance = $old_instance;
+
+      $instance['title']                       = strip_tags( $new_instance['title'] );
+      $instance['testimonials_number']         = strip_tags( $new_instance['testimonials_number'] );
+      $instance['order_testimonials_dropdown'] = strip_tags( $new_instance['order_testimonials_dropdown'] );
+
+      return $instance;
+    }
+
+    /**
+    * Widget Form
+    */
+    public function form ( $instance ) {
+       $instance_defaults = array(
+           'title'                       => 'Title',
+           'testimonials'                => '1',
+           'order_testimonials_dropdown' => 'asc',
+       );
+
+       $instance = wp_parse_args( $instance, $instance_defaults );
+
+       $title                       = esc_attr( $instance['title'] );
+       $testimonials_number         = esc_attr( $instance['testimonials_number'] );
+       $order_testimonials_dropdown = esc_attr( $instance['order_testimonials_dropdown'] );
+           ?>
+
+       <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( "Title:", 'rubase'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+       <p><label for="<?php echo $this->get_field_id('testimonials_number'); ?>"><?php _e( "Number of Testimonials:", 'rubase'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('testimonials_number'); ?>" name="<?php echo $this->get_field_name('testimonials_number'); ?>" type="number" min="0" value="<?php echo $testimonials_number; ?>" /></p>
+       <p><label for="<?php echo $this->get_field_id('order_testimonials_dropdown'); ?>"><?php _e( "Order Testimonials by:", 'rubase' ); ?></label></p>
+       <p>
+         <select name="<?php echo $this->get_field_name('order_testimonials_dropdown'); ?>" id="<?php echo $this->get_field_id('order_testimonials_dropdown'); ?>" class="widefat">
+           <option value="asc"<?php selected( $instance['order_testimonials_dropdown'], 'asc' ); ?>><?php _e( "Ascending", 'rubase' ); ?></option>
+           <option value="desc"<?php selected( $instance['order_testimonials_dropdown'], 'desc' ); ?>><?php _e( "Descending", 'rubase' ); ?></option>
+         </select>
+       </p>
+     <?php
+   }
+ }
+
+register_widget( 'RU_Testimonials_Widget' );
---
+++ wordpress/wp-content/plugins/ru-testimonials-plugin/ru-testimonials-plugin.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Plugin Name: RU Testimonials Plugin
+ * Description: Plugin for adding widgets for testimonials to your pages
+ * Version: 1.0
+ * License: GPL2
+*/
+
+define( 'PATH_INCLUDES', dirname( __FILE__ ) . '/inc' );
+define( 'PATH', dirname( __FILE__ ) );
+
+class RU_Testimonials_Plugin {
+  function __construct() {
+    //Register testimonials custom post type
+    add_action( 'init', array( $this, 'ru_testimonials_post_type' ), 5 );
+
+    //Register testimonials widget
+    add_action( 'widgets_init', array( $this, 'ru_testimonials_widget' ) );
+
+    //Register the shortcode
+    add_action( 'init', array( $this, 'ru_fetching_testimonials_shortcode' ) );
+
+    //Register testimonials help page
+    add_action( 'admin_menu', array( $this, 'ru_testimonials_help_page' ) );
+  }
+
+  // Include widget class
+  public function ru_testimonials_widget() {
+    include_once PATH_INCLUDES . '/ru-testimonials-widget-class.php';
+  }
+
+  // Include testimonials help page template
+  public function ru_testimonials() {
+    include_once( PATH_INCLUDES . '/testimonials-page-template.php' );
+  }
+
+  /**
+  * Register testimonials custom post type
+  */
+  public function ru_testimonials_post_type() {
+    register_post_type( 'testimonials', array(
+      'labels' => array(
+        'name'               => __("Testimonials ", 'rubase'),
+        'singular_name'      => __("Testimonial", 'rubase'),
+        'add_new'            => _x("Add New", 'testimonials', 'rubase' ),
+        'add_new_item'       => __("Add New Testimonial", 'rubase' ),
+        'edit_item'          => __("Edit Testimonial", 'rubase' ),
+        'new_item'           => __("New Testimonial", 'rubase' ),
+        'view_item'          => __("View Testimonial", 'rubase' ),
+        'search_items'       => __("Search Testimonials", 'rubase' ),
+        'not_found'          =>  __("No Testimonials found", 'rubase' ),
+        'not_found_in_trash' => __("No Testimonials found in Trash", 'rubase' ),
+      ),
+      'description'         => __("Testimonials for the demo", 'rubase'),
+      'public'              => true,
+      'publicly_queryable'  => true,
+      'query_var'           => true,
+      'rewrite'             => true,
+      'exclude_from_search' => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'menu_position'       => 40,
+      'supports' => array(
+        'title',
+        'editor',
+        'thumbnail',
+        'page-attributes',
+      ),
+    ));
+  }
+
+  /**
+  * Register a sample shortcode for fetching the testimonials
+  */
+  public function ru_fetching_testimonials_shortcode() {
+    add_shortcode( 'testimonials-shortcode', array( $this, 'ru_shortcode_body' ) );
+  }
+
+  /**
+  * Returns the content of the sample shortcode
+  */
+  public function ru_shortcode_body( $attr, $content = null ) {
+    $testimonials_args = array(
+      'post_status'    => 'publish',
+      'post_type'      => 'testimonials',
+      'posts_per_page' => '100',
+      'orderby'        => 'title',
+      'order'          => 'asc',
+    );
+    $query = new WP_Query( $testimonials_args );
+    ?>
+    <div class="wrapper">
+      <?php if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post() ?>
+          <ul>
+            <li>
+              <div class="post">
+                <a href="<?php the_permalink() ?>"
+                  <h3 class="title">
+                    <a href="<?php the_permalink() ?>" title=""><b><?php echo the_title() ?></b></a>
+                  </h3>
+                  <p><?php the_excerpt() ?></p>
+              </div>
+            </li>
+          </ul>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+    <?php
+  }
+
+  /**
+  * Callback for registering pages
+  */
+  public function ru_testimonials_help_page() {
+    add_menu_page(__( "Testimonials Help Page", 'rubase' ), __( "Testimonials Help Page", 'rubase' ), 'edit_themes', 'ru-testimonials', array( $this, 'ru_testimonials' ) );
+  }
+}
+
+$ru_testimonials_plugin = new RU_Testimonials_Plugin();
